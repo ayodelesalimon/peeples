faa5134ea3ec9f7fdac6921a9c6822536fbbfb01
fortune2009 <fortunekbz2009@gmail.com>
2021-06-04 21:43:43 +0100

    almost done

---
+++ lib/model/contact_model.dart
@@ -0,0 +1,55 @@
+
+enum Gender{ FEMALE, MALE }
+
+// Model for the unique contacts
+class Contact {
+  var id;
+  var image;
+  var firstName;
+  var lastName;
+  var company;
+  var title;
+  var mobile;
+  var gender;
+
+  Contact(
+      {
+        this.id, this.image,
+      this.firstName,
+      this.lastName,
+      this.company,
+      this.title,
+      this.mobile,
+      this.gender});
+
+  // Convert contact object into map object
+  Map<String, dynamic> toMap() {
+    var map = Map<String, dynamic>();
+    if (id != null) {
+      map['id'] = id;
+    }
+    map['image'] = image;
+    map['first_name'] = firstName;
+    map['last_name'] = lastName;
+    map['company'] = company;
+    map['title'] = title;
+    map['mobile'] = mobile;
+    map['gender'] = gender==Gender.MALE ? 'male' : 'female';
+    return map;
+  }
+
+  // TODO Extract from any map object
+Contact.fromMapObject(Map<String, dynamic> map) {
+    this.id = map['id'];
+    this.image = map['image'];
+    this.firstName = map['first_name'];
+    this.lastName = map['last_name'];
+    this.lastName = map['first_name'];
+    this.company = map['company'];
+    this.title = map['title'];
+    this.mobile = map['mobile'];
+    this.gender = map['gender'] == 'male' ? Gender.MALE : Gender.FEMALE;
+
+}
+
+}
--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,19 +1,125 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:list_tile_switch/list_tile_switch.dart';
+import 'package:peeples/model/contact_model.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({ Key key }) : super(key: key);
-
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  var dropping = [
+    'All contacts',
+    'Family',
+    'Friends',
+  ];
+
+  bool isThemeBGColor = false;
+
   @override
   Widget build(BuildContext context) {
+    var dropHolder = dropping[0];
     return Scaffold(
-      body: Center(
-        child: Text("data"),
-      ),
-    );
+        backgroundColor: isThemeBGColor ? Colors.black : Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          shadowColor: Colors.transparent,
+          title: Padding(
+            padding: const EdgeInsets.only(right: 130),
+            child: DropdownButtonHideUnderline(
+              child: DropdownButton(
+                isExpanded: true,
+                value: dropHolder,
+                items: dropping
+                    .map((e) => DropdownMenuItem<String>(
+                          value: e,
+                          child: Text(
+                            e,
+                            style: TextStyle(
+                              color:
+                                  isThemeBGColor ? Colors.white : Colors.black,
+                            ),
+                          ),
+                        ))
+                    .toList(),
+                onChanged: (value) {
+                  setState(() {
+                    //TODO I tried changing the value but it is not wrking
+                    dropHolder = value.toString();
+                  });
+                },
+              ),
+            ),
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20),
+              child: Text(
+                'Dark',
+                style: TextStyle(
+                    color: isThemeBGColor ? Colors.white : Colors.black),
+              ),
+            ),
+            Switch(
+                value: isThemeBGColor,
+                onChanged: (val) {
+                  setState(() {
+                    isThemeBGColor = val;
+                  });
+                })
+          ],
+        ),
+        body: ListView.separated(
+          physics: BouncingScrollPhysics(),
+            itemBuilder: (context, index) {
+              return ListTile(
+                leading: ClipRect(
+                  child: Image.asset(list[index].image),
+                ),
+                title: Text(
+                  list[index].firstName,
+                  style: TextStyle(
+                      color: isThemeBGColor ? Colors.white : Colors.black),
+                ),
+                subtitle: Text(
+                  list[index].mobile,
+                  style: TextStyle(
+                      color: isThemeBGColor ? Colors.grey : Colors.grey),
+                ),
+                trailing: Icon(
+                  Icons.arrow_forward_ios_rounded,
+                  size: 12,
+                  color: isThemeBGColor ? Colors.grey : Colors.grey,
+                ),
+              );
+            },
+            separatorBuilder: (context, index) => Divider(
+                  color: isThemeBGColor ? Colors.grey : Colors.grey,
+                ),
+            itemCount: list.length));
   }
 }
+
+var list = [
+  Contact(
+      image: 'assets/images/avatarPic.png',
+      firstName: 'Ayodele',
+      mobile: '09087875784'),
+  Contact(
+      image: 'assets/images/avatarPic.png',
+      firstName: 'David',
+      mobile: '09087875784'),
+  Contact(
+      image: 'assets/images/avatarPic.png',
+      firstName: 'Fortune',
+      mobile: '09087875784'),
+  Contact(
+      image: 'assets/images/avatarPic.png',
+      firstName: 'Fortune',
+      mobile: '09087875784'),
+  Contact(
+      image: 'assets/images/avatarPic.png',
+      firstName: 'Fortune',
+      mobile: '09087875784'),
+];
